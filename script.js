f899be56d10852577897134cd3ad6b0a48c72442
seolacarrano <seola6@gmail.com>
2021-02-27 13:33:45 -0500

    fixed wordHolder

--- script.js
+++ script.js
@@ -10,50 +10,56 @@ let wordToGuess;
 
 // displaying words with underscore when clicking start btn
 function guessedWord() {
+    console.log(word)
     wordToGuess = word.split('').map(letter => {
-        if (letter === 'r' || letter === 's' || letter === 't' || letter === 'l' || letter === 'n' || letter === 'e') {
-            return letter = letter
-        } else {
-            return letter = " _ "
+        // updating wordHolder 
+        for (let i = 0; i < guessed.length; i++) {
+            if (guessed[i] === letter) {
+                return letter
+            }
         }
-    }).join(' ');
+
+        // start setting
+        if (letter === 'r' || letter === 's' || letter === 't' || letter === 'l' || letter === 'n' || letter === 'e') {
+            return letter
+        } else {
+            return " _ "
+        }
+    }).join('');
 
     wordHolder.innerText = wordToGuess;
     wordHolder.style.textAlign = "center";
     wordHolder.style.fontSize = "50px"
-
+    alert(word)
     //wordHolder.innerText = word;
 }
 startBtn.addEventListener("click", guessedWord)
 
 
+// check if the input includes 3 consonants and 1 vowel
 function handlingGuess(e) {
-    // check if the input includes 3 consonants and 1 vowel
-    let vowel = userGuess.value.match(/[aeiou]/g)
-    let consonant = userGuess.value.match(/[bcdfghjklmnpqrstvwxz]/g)
+    const vowel = userGuess.value.match(/[aeiou]/g)
+    const consonant = userGuess.value.match(/[bcdfghjklmnpqrstvwxz]/g)
     let include = userGuess.value.split('').filter(userVal => word.includes(userVal))
 
     //if input includes one vowel and three consonants, check the validity
-    if (vowel.length === 1 && consonant.length === 3) {
+    if (vowel.length <= 1 || consonant.length <= 3) {
         let feedback = document.createElement('h3')
         if (include.length !== 0) {
             feedback.innerText = `This word includes ${include}`
             feedbackDiv.appendChild(feedback)
 
-            // //change _ to guessed letter 
-            // wordToGuess = word.split('').map(letter => {
-            //     if (letter == include || letter === 'r' || letter === 's' || letter === 't' || letter === 'l' || letter === 'n' || letter === 'e') {
-            //         return letter = letter
-            //     } else {
-            //         return letter = " _ "
-            //     }
-            // }).join(' ');
-            // wordHolder.innerText = wordToGuess;
-            guessed.indexOf(include) === -1 ? guessed.push(include) : null;
-            console.log(guessed.indexOf(include))
-            if (word.indexOf(include) >= 0) {
+            //update wordHolder
+            if (guessed.indexOf(include) === -1) {
+                include.forEach(e => {
+                    guessed.push(e)
+                })
                 guessedWord();
+                checkIfGameWon();
             }
+            // if (guessed.indexOf(include) >= 0) {
+            //     guessedWord();
+            // }
 
         } else {
             feedback.innerText = `This word does not include ${userGuess.value}`
@@ -62,10 +68,17 @@ function handlingGuess(e) {
 
     } else {
         // if input does not include one vowel and three consonants, alert 
-        alert('Please enter 3 consonants and 1 vowel')
+        alert('Please enter less than 4 consonants and less than 2 vowels')
     }
 
     e.preventDefault();
 }
+
+function checkIfGameWon() {
+    if (wordToGuess === word) {
+        alert('You Got It!!!');
+    }
+}
+
 submitBtn.addEventListener("click", handlingGuess)
 
